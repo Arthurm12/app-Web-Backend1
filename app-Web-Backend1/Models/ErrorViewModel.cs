581efc32de9b8bec837a495b0c336857b774d753
Arthur Machado <ahfmexecutivo@gmail.com>
2022-10-10 15:36:32 -0300

    Adicionar arquivos de projeto.

---
+++ app-Web-Backend1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace app_Web_Backend1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
